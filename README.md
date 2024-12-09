e22c883ada56043a7a674db4544524adf97b479e
SL <sl@qshare.io>
2024-12-09 12:09:27 +0100

    Updated the php function and readme file to support component

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Kastellet-JavaScript-loader
+
+
+## structure to add additional component
+theme-folder/
+│
+├── js/
+│   ├── loader.js
+│   ├── component1.js
+│   ├── component2.js
+│
+└── functions.php
+
+import { myFunction } from './component1.js';
+import { anotherFunction } from './component2.js';
+
+myFunction();
+anotherFunction();
--- functions.php
+++ functions.php
@@ -1,18 +1,26 @@
-// function that allows .JSON files to be uploaded in the media folder
+// Allow .JSON files to be uploaded in the media folder
 function allow_json_mime($mimes) {
-	$mimes['json'] = 'application/json';
-	return $mimes;
-	}
+    $mimes['json'] = 'application/json';
+    return $mimes;
+}
 add_filter('upload_mimes', 'allow_json_mime');
 
-// loads the JavaScript and .JSON files into wordpress
+// Enqueue the main JavaScript file with ES6 module support
 function enqueue_custom_script() {
-    // Enqueue your custom JS file
-    wp_enqueue_script('custom-filter-script', get_template_directory_uri() . '/js/loader.js', array('jquery'), null, true);
-    
-    // Find and load the .JSON files
-    wp_localize_script('custom-filter-script', 'jsonData', array(
-        'json_url' => 'https://staging-1732022352.gbplayground.dk/wp-content/uploads/2024/11/dk.json',
-    ));
+    wp_enqueue_script(
+        'loader-script', // Handle for the script
+        get_template_directory_uri() . '/js/loader.js', // Path to loader.js
+        array(), // Dependencies (empty since this is the main module)
+        null, // No version number
+        true // Load in the footer
+    );
+
+    // Add type="module" for ES6 module support
+    add_filter('script_loader_tag', function($tag, $handle, $src) {
+        if ('loader-script' === $handle) {
+            $tag = '<script type="module" src="' . esc_url($src) . '"></script>';
+        }
+        return $tag;
+    }, 10, 3);
 }
 add_action('wp_enqueue_scripts', 'enqueue_custom_script');
